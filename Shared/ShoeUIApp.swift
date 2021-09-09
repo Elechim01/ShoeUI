44391b95397ecdaf92900e02cbe10cf9e7f2094d
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-09 23:41:35 +0200

    Initial Commit

---
+++ Shared/ShoeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeUIApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ShoeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
